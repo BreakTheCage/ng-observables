fcc1a8f3846cc19221dba2ec85f6527fa17bc1a3
BreakTheCage <sarbaztanha707@outlook.com>
2019-06-25 12:20:05 +0400

    #Section 13: Completed; ngObservable; End of 177

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatdSub: Subscription;
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.activatdSub = this.userService.activatedEmitter.subscribe(didActivated => {
+      this.userActivated = didActivated;
+    })
+  }
+  ngOnDestroy(): void {
+    this.activatdSub.unsubscribe();
+  }
+
+
 }
